d69e0609441dd0501461ce1d79119f74a837a51a
Administrator <guiyuzhu@qq.com>
2020-08-11 16:47:35 +0800

    测试版本

--- src/main/java/edu/wdit/test/Test.java
+++ src/main/java/edu/wdit/test/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world.");
     }
+
+    public static void versionControl() {
+        //测试更新内容
+    }
 }
